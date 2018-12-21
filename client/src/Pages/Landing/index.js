66cda69b7fcb7b653983328d47c95fc18191fd45
Wade Beall <wadegb12@okstate.edu>
2018-12-20 21:54:03 -0600

    responsive image

--- client/src/Pages/Landing/index.js
+++ client/src/Pages/Landing/index.js
@@ -3,7 +3,7 @@ import './landing.css'
 import { Page, Footer} from '../../Components'
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import photo from '../../Assets/Header.jpg';
+import photo from '../../Assets/HeaderV2.jpg';
 import { Typography } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import ButtonBase from '@material-ui/core/ButtonBase';
@@ -24,22 +24,23 @@ const styles = theme => ({
     image: {
       position: 'relative',
       height: 200,
-      [theme.breakpoints.down('xs')]: {
-        width: '100% !important', // Overrides inline-style
-        height: 100,
-      },
-      '&:hover, &$focusVisible': {
-        zIndex: 1,
-        '& $imageBackdrop': {
-          opacity: 0.15,
-        },
-        '& $imageMarked': {
-          opacity: 0,
-        },
-        '& $imageTitle': {
-          border: '4px solid currentColor',
-        },
-      },
+      
+    //   [theme.breakpoints.down('xs')]: {
+    //     width: '100% !important', // Overrides inline-style
+    //     height: 100,
+    //   },
+    //   '&:hover, &$focusVisible': {
+    //     zIndex: 1,
+    //     '& $imageBackdrop': {
+    //       opacity: 0.15,
+    //     },
+    //     '& $imageMarked': {
+    //       opacity: 0,
+    //     },
+    //     '& $imageTitle': {
+    //       border: '4px solid currentColor',
+    //     },
+    //   },
     },
     focusVisible: {},
     imageButton: {
@@ -53,94 +54,97 @@ const styles = theme => ({
       justifyContent: 'center',
       color: theme.palette.common.white,
     },
-    imageSrc: {
+    responsiveImg: {
       position: 'absolute',
       left: 0,
       right: 0,
       top: 0,
       bottom: 0,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center 40%',
-    },
-    imageBackdrop: {
-      position: 'absolute',
-      left: 0,
-      right: 0,
-      top: 0,
-      bottom: 0,
-      backgroundColor: theme.palette.common.black,
-      opacity: 0.4,
-      transition: theme.transitions.create('opacity'),
+      width: '100%',
+      height: '100%'
     },
     imageTitle: {
       position: 'relative',
       padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 4}px ${theme.spacing.unit + 6}px`,
     },
+    landingPhotoContainer: {
+        position: 'relative',
+        maxWidth: '100%',
+        maxHeight: '100vh'
+    }
   });
 
-  const images = [
+  const image = [
     {
       url: photo,
-      title: 'Quote',
-      width: '10%',
+      title: 'Get a Quote',
+      location: 'Oklahoma City, OK',
+      width: '100%',
+    //   height: '50%'
     },
   ];
 
+const ResponsiveImage = ({ src, width, height, containerClass, imgClass }) => (
+    <div className={containerClass} style={{width}} >
+    <div style={{ paddingBottom:'55%' }} />
+      <img
+        src={src}
+        className={imgClass}
+      />
+    </div>
+);
 
+const getHeightFromWidth = width =>
+    Math.floor(5.5 * (width / 10));
 
-const Landing = (props) => {
-    const { history, classes } = props
-    
+const Landing = ({ history, classes }) => {
     return (
         <Page history={history}>
-            {/* <div style={{ textAlign:"center"}}> 
-                <Typography style={{ fontSize: 40, padding: 10}}> 
-                        Let us 
-                        <Button variant="outlined" color="primary" >
-                            cater
-                        </Button>
-                        your event 
-                    <Typography style={{ fontSize: 20, padding: 10}}> 
-                         Oklahoma City, OK
-                    </Typography>
-                </Typography>
-                
-                 <img src={photo} width="100%" height="50%" />
-            </div>   */}
-            
-                <div className={classes.root}>
-                {images.map(image => (
-                <ButtonBase
+            <ResponsiveImage
+                src={photo}
+                width={100000}
+                // height={getHeightFromWidth(window.innerWidth)}
+                containerClass={classes.landingPhotoContainer}
+                imgClass={classes.responsiveImg}
+            />
+        
+        {/* <span
+            className={classes.imageSrc}
+            style={{ backgroundImage: `url(${image.url})`, }}
+        /> */}
+            {/* <div className={classes.root}>
+                 {images.map(image => (  
+                 <ButtonBase 
                     focusRipple
-                    key={image.title}
-                    className={classes.image}
-                    focusVisibleClassName={classes.focusVisible}
-                    style={{
-                    width: image.width,
-                    }}
-                >
+                     key={image.title}
+                     className={classes.image}
+                     focusVisibleClassName={classes.focusVisible}
+                     style={{ width: image.width, }}
+                > 
                     <span
-                    className={classes.imageSrc}
-                    style={{
-                        backgroundImage: `url(${image.url})`,
-                    }}
+                        className={classes.responsiveImg}
+                        style={{ backgroundImage: `url(${image.url})`, }}
                     />
-                    <span className={classes.imageBackdrop} />
                     <span className={classes.imageButton}>
-                    <Typography
-                        component="span"
-                        variant="subtitle1"
-                        color="inherit"
-                        className={classes.imageTitle}
-                    >
-                        {image.title}
-                    </Typography>
+                        <Typography className={classes.imageTitle}> {image.title}Get a </Typography>
+                        <Button>
+                        <Typography
+                            component="span"
+                            variant="subtitle1"
+                            color="inherit"
+                            className={classes.imageTitle}
+                        >
+                            {image.title}
+                        </Typography>
+                        </Button>
                     </span>
-                </ButtonBase>
-                ))}
-            </div>
+                    <span className={classes.imageButton}>
+                    </span>
+                 </ButtonBase>  
+                ))} 
+            </div> */}
             <Footer />
-         </Page>
+        </Page>
     )
 }
 
@@ -149,8 +153,3 @@ Landing.propTypes = {
 };
 
 export default withStyles(styles)(Landing);
-
-
-
-
-
