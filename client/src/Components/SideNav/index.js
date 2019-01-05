5d0ccd342b8353b6835ceb95d4a6d57e7a789869
wadegb12 <wadegb@okstate.edu>
2019-01-04 23:29:31 -0600

    added moravian logo to side nav

--- client/src/Components/SideNav/index.js
+++ client/src/Components/SideNav/index.js
@@ -5,6 +5,7 @@ import List from '@material-ui/core/List';
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
+import logo from '../../Assets/Logo.png';
 
 const styles = {
 };
@@ -15,6 +16,9 @@ const SideNav = ({ user, history }) => {
             component="nav" 
             style={styles.root}
         >   
+            <div>
+                <img src={logo} width="50" height="50" style={{margin:"0 10px 0 0"}}/>
+            </div>
             <Button component={Link} to="/">
                 <Typography> Home </Typography>
             </Button>
