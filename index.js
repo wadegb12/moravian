c681872086f0e7586cfc74cef2001a30cc9e094a
wadegb12 <wadegb@okstate.edu>
2019-01-05 18:43:10 -0600

    send email from form

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 app.post('/api/form', (req, res) => {
     nodemailer.createTestAccount((err,account) => {
-        const htmlEmail = "
+        const htmlEmail = `
             <h3>Contact Details</h3>
             <ul>
                 <li>Name: ${req.body.name}</li>
@@ -16,6 +16,39 @@ app.post('/api/form', (req, res) => {
             </ul>
             <h3>Message</h3>
             <p>${req.body.message}</p>
-        "
+        `
+
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: '',
+                pass: ''
+            }
+        })
+
+        let mailOptions = {
+            from: '',
+            to: '',
+            replyto: '',
+            subject: 'Quote Requested',
+            text: req.body.message,
+            html: htmlEmail
+        }
+
+        transporter.sendMail(mailOptions, (err, info) => {
+            if(err) {
+                return console.log(err)
+            }
+
+            console.log('Message sent: %s', info.message)
+            console.logt('Message URL: %s', nodemailer.getTestMessageUrl(info))
+        })
     })
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log('Server listening on port ${PORT}')
 })
