e39190e837e5b13e75a18d8e005bb1fe6193fedf
wadegb12 <wadegb@okstate.edu>
2019-01-06 19:06:36 -0600

    footer with instagram and facebook links

--- client/src/Components/Footer/index.js
+++ client/src/Components/Footer/index.js
@@ -1,21 +1,40 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-
+import instagramLogo from '../../Assets/instagram.png';
+import facebookLogo from '../../Assets/facebookLogo.png';
 
 const styles = theme => ({
     footer: {
-        backgroundColor: theme.palette.background.paper,
-        padding: theme.spacing.unit * 6,
+        backgroundColor: "#323031",
+        // backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing.unit * 4,
     },
+    footerTextColor: {
+        color: "#DCDCDE"
+    },
+    imageSize: {
+        width: "25px",
+        height: "25px"
+    }
 })
 
 const Footer = ({classes }) => {
 
     return (<footer className={classes.footer}>
-        <Typography variant="title" align="center" gutterBottom>
-            Contact Us
-        </Typography>
+        <div className="Footer" align="center" >
+            <Typography className={classes.footerTextColor} variant="title" gutterBottom>
+                Contact Us
+            </Typography>
+            <div>
+                <a href="https://www.instagram.com/moraviancoffee/">
+                    <img src={instagramLogo} className={classes.imageSize} style={{margin:"0 10px 0 0"}}/>
+                </a>
+                <a href="https://www.facebook.com/MoravianCoffee/">
+                    <img src={facebookLogo} className={classes.imageSize}/>
+                </a>
+            </div>
+        </div>
     </footer>)
 }
 
