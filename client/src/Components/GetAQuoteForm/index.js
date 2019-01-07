0492c7cf47d7b7f6825c4e8e4df67b1c9f77fe51
wadegb12 <wadegb@okstate.edu>
2019-01-06 22:42:45 -0600

    removed component and added dropdowns with options

--- client/src/Components/GetAQuoteForm/index.js
+++ client/src/Components/GetAQuoteForm/index.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import { Form, FormGroup, Input, Label, Button} from 'reactstrap'
 import axios from 'axios'
-import CustomDropdown from '../CustomDropdown'
 
 const styles = {
     formWidth: {
@@ -70,8 +69,98 @@ class GetAQuoteForm extends Component {
                         onChange={this.handleChange} />
                 </FormGroup>
                 <FormGroup>
-                    <Label for="numAttendees"> Number of Attendees: </Label>
-                    <CustomDropdown/>
+                    <Label for="numAttendees"> Start Time: </Label>
+                    <Input 
+                        type="select" 
+                        name="numAttendees"
+                        onChange={this.handleChange} 
+                    >
+                        <option></option>
+                        <option>1:00</option>
+                        <option>2:00</option>
+                        <option>3:00</option>
+                        <option>4:00</option>
+                        <option>5:00</option>
+                        <option>6:00</option>
+                        <option>7:00</option>
+                        <option>8:00</option>
+                        <option>9:00</option>
+                        <option>10:00</option>
+                        <option>11:00</option>
+                        <option>12:00</option>
+                    </Input>
+                </FormGroup>
+                <FormGroup>
+                    <Input 
+                        type="select"
+                        name="startTimeAMPM"
+                        onChange={this.handleChange} 
+                    >
+                        <option></option>
+                        <option>AM</option>
+                        <option>PM</option>
+                    </Input>
+                </FormGroup>
+                <FormGroup>
+                    <Label for="numAttendees"> End Time: </Label>
+                    <Input 
+                        type="select"
+                        name="numAttendees"
+                        onChange={this.handleChange} 
+                    >
+                        <option></option>
+                        <option>1:00</option>
+                        <option>2:00</option>
+                        <option>3:00</option>
+                        <option>4:00</option>
+                        <option>5:00</option>
+                        <option>6:00</option>
+                        <option>7:00</option>
+                        <option>8:00</option>
+                        <option>9:00</option>
+                        <option>10:00</option>
+                        <option>11:00</option>
+                        <option>12:00</option>
+                    </Input>
+                </FormGroup>
+                <FormGroup>
+                    <Input 
+                        type="select"
+                        name="endTimeAMPM"
+                        onChange={this.handleChange} 
+                    >
+                        <option></option>
+                        <option>AM</option>
+                        <option>PM</option>
+                    </Input>
+                </FormGroup>
+                <FormGroup>
+                    <Label for="numAttendees"> Number of Attendees </Label>
+                    <Input 
+                        type="select"
+                        name="numAttendees"
+                        onChange={this.handleChange} 
+                    >
+                        <option></option>
+                        <option>0-50</option>
+                        <option>50-100</option>
+                        <option>100-150</option>
+                        <option>150-200</option>
+                        <option>200+</option> 
+                    </Input>
+                </FormGroup>
+                <FormGroup>
+                    <Label for="bar"> Type of Bar </Label>
+                    <Input 
+                        type="select"
+                        name="bar"
+                        onChange={this.handleChange} 
+                    >
+                        <option></option>
+                        <option>Espresso</option>
+                        <option>Pour Over</option>
+                        <option>Both</option>
+                    </Input>
                 </FormGroup>
                 <FormGroup>
                     <Label for="message"> Comments: </Label>
