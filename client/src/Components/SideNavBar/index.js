615ca6e2246405d2735512d9b09fdc1220c58b3d
wadegb12 <wadegb@okstate.edu>
2019-01-09 21:18:43 -0600

    side nav bar icon and logo placement

--- client/src/Components/SideNavBar/index.js
+++ client/src/Components/SideNavBar/index.js
@@ -5,18 +5,30 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import MenuIcon from '@material-ui/icons/Menu';
 import Button from '@material-ui/core/IconButton';
+import logo from '../../Assets/moravianLogo.png';
 
 const styles = {
     sideNavBar: {
         height: "70px",
         background: "#323031",
     },
+    sideNavBarWidth: {
+        marginRight: '5%',
+        marginLeft: '5%'
+    },
+    icon: {
+        top: '10px'
+    },
     iconColor: {
-        color: "white",
+        color: 'white',
     },
     sideNav: {
-      width: "100%",
-      maxWidth: "50%",
+      width: '100%',
+      maxWidth: '50%',
+    },
+    logo: {
+        float: 'right',
+        margin: '10px',
     }
 };
 
@@ -43,9 +55,13 @@ class SideNavBar extends React.Component {
                     onSetOpen={this.onSetSidebarOpen}
                     styles={{ sidebar: styles.sideNav} }
                 >
-                    <Button onClick={() => this.onSetSidebarOpen(true)}>
-                        <MenuIcon style ={styles.iconColor}/>
-                    </Button>
+                    <div style={styles.sideNavBarWidth}>
+                        <Button onClick={() => this.onSetSidebarOpen(true)} style={styles.icon}>
+                            <MenuIcon style ={styles.iconColor}/>
+                        </Button>
+
+                        <img src={logo} width="50" height="50" style={styles.logo}/>
+                    </div>
                 </Sidebar>
             </AppBar>
         );
