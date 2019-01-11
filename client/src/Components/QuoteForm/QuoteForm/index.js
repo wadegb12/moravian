8afd7214dd060ccd8c825f159e4165999f81a9a6
wadegb12 <wadegb@okstate.edu>
2019-01-11 17:57:44 -0600

    moved styles to styles js file

--- client/src/Components/QuoteForm/QuoteForm/index.js
+++ client/src/Components/QuoteForm/QuoteForm/index.js
@@ -4,40 +4,7 @@ import { Button, Modal, ModalHeader} from 'reactstrap'
 import axios from 'axios'
 import Media from "react-media";
 import { Link } from 'react-router-dom'
-
-const styles = {
-    formContainer: {
-        padding: '20px',
-        maxWidth: '600px',
-        margin: 'auto'
-    },
-    grayBackground: {
-        background: '#fafafa'
-    },
-    inputFieldMaxWidth: {
-        maxWidth: '130px',
-        background: '#fafafa',
-    },
-    dateMaxWidth: {
-        maxWidth: '190px',
-        background: '#fafafa',
-    },
-    inputAmPm: {
-        maxWidth: '100px',
-    },
-    labelMaxWidth: {
-        maxWidth: '110px'
-    },
-    quoteText: {
-        fontSize: '24px'
-    },
-    modalContainerWidth: {
-        marginRight: '5%',
-        marginLeft: '5%'
-    },
-};
-
-
+import styles from './styles'
 
 class QuoteForm extends Component {
     constructor() {
---
+++ client/src/Components/QuoteForm/QuoteForm/styles.js
@@ -0,0 +1,33 @@
+const styles = {
+    formContainer: {
+        padding: '20px',
+        maxWidth: '600px',
+        margin: 'auto'
+    },
+    grayBackground: {
+        background: '#fafafa'
+    },
+    inputFieldMaxWidth: {
+        maxWidth: '130px',
+        background: '#fafafa',
+    },
+    dateMaxWidth: {
+        maxWidth: '190px',
+        background: '#fafafa',
+    },
+    inputAmPm: {
+        maxWidth: '100px',
+    },
+    labelMaxWidth: {
+        maxWidth: '110px'
+    },
+    quoteText: {
+        fontSize: '24px'
+    },
+    modalContainerWidth: {
+        marginRight: '5%',
+        marginLeft: '5%'
+    },
+};
+
+export default styles;
