a439b8239a4a42f384cec7449a43394f4856df89
wadegb12 <wadegb@okstate.edu>
2019-01-11 18:05:32 -0600

    uremoved toolbar and sidebar components, and  added toggle drawer

--- client/src/Components/SideNavBar/index.js
+++ client/src/Components/SideNavBar/index.js
@@ -1,30 +1,23 @@
 import React from "react";
-import Sidebar from "react-sidebar";
 import SideNav from "../SideNav";
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import MenuIcon from '@material-ui/icons/Menu';
 import Button from '@material-ui/core/IconButton';
 import logo from '../../Assets/moravianLogo.png';
 
 const styles = {
     sideNavBar: {
-        height: "70px",
         background: "#323031",
     },
     sideNavBarWidth: {
         marginRight: '5%',
-        marginLeft: '5%'
+        marginLeft: '5%',
     },
     icon: {
         top: '10px'
     },
     iconColor: {
         color: 'white',
-    },
-    sideNav: {
-      width: '100%',
-      maxWidth: '50%',
     },
     logo: {
         float: 'right',
@@ -36,33 +29,31 @@ class SideNavBar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            sidebarOpen: false
+            left: false,
         };
-        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
     }
 
-    onSetSidebarOpen(open) {
-        this.setState({ sidebarOpen: open });
-    }
+    toggleDrawer = (side, open) => () => {
+        this.setState({
+            [side]: open,
+        });
+    };
 
     render() {
+
+        console.log(this.state)
         return (
             <AppBar position="static">
-                <Toolbar style={styles.sideNavBar}/>
-                <Sidebar
-                    sidebar={<SideNav/>}
-                    open={this.state.sidebarOpen}
-                    onSetOpen={this.onSetSidebarOpen}
-                    styles={{ sidebar: styles.sideNav} }
-                >
+                <div style={styles.sideNavBar}>
                     <div style={styles.sideNavBarWidth}>
-                        <Button onClick={() => this.onSetSidebarOpen(true)} style={styles.icon}>
+                        <SideNav open={this.state.left} toggleDrawer={this.toggleDrawer}/>
+                        <Button open={this.state.left} onClick={this.toggleDrawer('left', true)} style={styles.icon}>
                             <MenuIcon style ={styles.iconColor}/>
                         </Button>
 
                         <img src={logo} alt="" width="50" height="50" style={styles.logo}/>
                     </div>
-                </Sidebar>
+                </div>
             </AppBar>
         );
     }
