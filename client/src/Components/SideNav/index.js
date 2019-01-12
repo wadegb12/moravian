8f00f17139281852e94f2769cf87567bab853c83
wadegb12 <wadegb@okstate.edu>
2019-01-11 18:13:24 -0600

    side nav that doenst hide components under it

--- client/src/Components/SideNav/index.js
+++ client/src/Components/SideNav/index.js
@@ -1,48 +1,77 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { Link } from 'react-router-dom'
+import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
-import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import { Link } from 'react-router-dom'
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import logo from '../../Assets/moravianLogo.png';
 
 const styles = {
-    root: {
-        height: "100%",
-        background: "#323031"
-    },
-    backgroundColor: {
-        width: "100%",
-        background: "#323031",
-    },
-    whiteColor: {
-        color: "#ffffff"
-    },
+  list: {
+    
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+  whiteColor: {
+      color: "#ffffff"
+  },
+  root:{
+    height: '100%',
+    background: "#323031",
+  }
 };
 
-const SideNav = ({ classes }) => {
-    return (
-        <List 
-            component="nav" 
-            style={styles.root}
-        >   
-            <div align="center" style={{margin:"10px"}}>
-                <img src={logo} alt="" width="144" height="144" />
-            </div>
-            <Button component={Link} to="/">
-                <Typography className={classes.whiteColor}> Home </Typography>
-            </Button>
-            {/* <Divider/>
-            <Button component={Link} to="/AboutUs">
-                <Typography className={classes.whiteColor}> About Us </Typography>
-            </Button> */}
-            <Divider/>
-            <Button component={Link} to="/GetAQuote">
-                <Typography className={classes.whiteColor}> Get A Quote </Typography>
-            </Button>
+const TemporaryDrawer = (props) => {
+
+    const { classes } = props;
+
+    const sideList = (
+      <div className={classes.list}>
+        <List style={styles.root} >   
+          <div align="center" style={{margin:"10px"}}>
+              <img src={logo} alt="" width="144" height="144" />
+          </div>
+          <Button component={Link} to="/">
+              <Typography className={classes.whiteColor}> Home </Typography>
+          </Button>
+          <Divider/>
+          <Button component={Link} to="/GetAQuote">
+              <Typography className={classes.whiteColor}> Get A Quote </Typography>
+          </Button>
         </List>
+      </div>
+    );
+
+    return (
+      <div>
+        <Drawer open={props.open} onClose={props.toggleDrawer('left', false)}>
+          <div
+            style={styles.root}
+            tabIndex={0}
+            role="button"
+            onClick={props.toggleDrawer('left', false)}
+            onKeyDown={props.toggleDrawer('left', false)}
+          >
+            {sideList}
+          </div>
+        </Drawer>
+      </div>
     );
 }
 
-export default withStyles(styles)(SideNav);
+TemporaryDrawer.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(TemporaryDrawer);
+
