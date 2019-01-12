5cf7a28be08094022f7241515fa4f14f3ea55746
wadegb12 <wadegb@okstate.edu>
2019-01-11 18:14:33 -0600

    removed import statements

--- client/src/Components/SideNav/index.js
+++ client/src/Components/SideNav/index.js
@@ -4,11 +4,6 @@ import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import { Link } from 'react-router-dom'
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
