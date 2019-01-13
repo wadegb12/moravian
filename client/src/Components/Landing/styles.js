e6ef9f83f95b1b1b56f5f9fffb001e6b36c0a30b
wadegb12 <wadegb@okstate.edu>
2019-01-13 01:43:19 -0600

    removed unused classes from landing page component styles

--- client/src/Components/Landing/styles.js
+++ client/src/Components/Landing/styles.js
@@ -20,61 +20,6 @@ const styles = ({
         width: '100%',
         alignSelf: 'center',
     },
-
-
-
-
-
-    responsiveImg: {
-        width:'100%',
-    },
-    vertialImg: {
-        width: '100%',
-        // maxWixdth:'600px',
-        // height: '100vh',
-    },
-    largeImageContainer: {
-        width: '50%',
-        background: "#323031"
-    },
-    image: {
-        padding: '20px',
-        width: '100%'
-    },
-    flex: {
-        display: 'flex'
-    },
-    background: {
-        background: "#323031"
-    },
-    quoteTextContainer: {
-        position: 'absolute',
-        top: '10%',
-        width: '100%'
-    },
-    button: {
-        border: '2px solid',
-    },
-    infoText: {
-        fontSize:'20px',
-        padding: '5px'
-    },
-    basedText: {
-        fontSize:'20px',
-        paddingTop: '100px',
-        paddingBottom: '10px'
-    },
-    coffeeShopText: {
-        fontSize: '20px',
-        paddingTop: '30px'
-    },
-    getQuoteText: {
-        fontSize: '26px',
-        padding: '5px'
-    },
-    buttonPadding: {
-        padding: '10px'
-    }
 });
 
 export default styles;
