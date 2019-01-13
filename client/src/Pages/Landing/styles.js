64934a0742ff9bb94b13b744fdd3349bfdb3c092
wadegb12 <wadegb@okstate.edu>
2019-01-13 01:50:22 -0600

    removed unused classes

--- client/src/Pages/Landing/styles.js
+++ client/src/Pages/Landing/styles.js
@@ -1,44 +1,6 @@
 const styles = ({
-    imageContainer: {
-        overflow: 'hidden',
-        width: '100%',
-        maxHeight: '90vh'
-    },
-    imageHolder: {
-        display: 'flex',
-        height: '100%',
-        alignItem: 'center'
-    },
-    imageItself: {
-        height: 'auto',
-        width: '100%',
-        alignSelf: 'center',
-    },
-    responsiveImg: {
-        width:'100%',
-    },
-    vertialImg: {
-        width: '100%',
-        // maxWixdth:'600px',
-        // height: '100vh',
-    },
-    largeImageContainer: {
-        width: '50%',
-        background: "#323031"
-    },
-    image: {
-        padding: '20px',
-        width: '100%'
-    },
-    flex: {
-        display: 'flex'
-    },
-    background: {
-        background: "#323031"
-    },
-    quoteTextContainer: {
-        position: 'absolute',
-        top: '10%',
+    landingPageText: {
+        top: '30%',
         width: '100%'
     },
     button: {
@@ -50,30 +12,10 @@ const styles = ({
         padding: '5px',
         color: '#000000'
     },
-    basedText: {
-        fontSize:'32px',
-        paddingTop: '100px',
-        paddingBottom: '10px',
-        color: '#000000'
-    },
-    coffeeShopText: {
-        fontSize: '20px',
-        paddingTop: '30px'
-    },
     getQuoteText: {
         fontSize: '42px',
         padding: '5px',
         color: '#000000'
-    },
-    buttonPadding: {
-        padding: '10px'
-    },
-    landingPageText: {
-        top: '30%',
-        width: '100%'
-    },
-    test: {
-        positon: 'relative'
     },
     smallButton: {
         border: '2px solid',
